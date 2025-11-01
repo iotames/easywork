faebc63366322586b407e1edf15dbba7e35d5011
Hankin <hankin@catmes.com>
2025-11-02 00:42:18 +0800

    UPDATE .vuepress/config.ts defineUserConfig.base

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -5,7 +5,8 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme(),
-//   base: '/',
+  // base默认值为 '/'
+  base: '/easywork/',
   lang: 'zh-CN',
   title: "沙滩星空的博客",
   description: "金于沙里得，玉向石中求。个人博客主要是工作和学习的备忘笔记，包含开发与运维的提效脚本工具。",
