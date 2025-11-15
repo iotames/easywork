bab86a1bfbc505685ef1726eabe00bb4ec789f24
iotames <iotames@users.noreply.github.com>
2025-11-15 09:20:31 +0000

    deploy: ecd76561fb5ce12e1d6505e1d05832093870fa57

---
+++ assets/inoutputs.html-BCxPsPOS.js
@@ -0,0 +1,73 @@
+import{_ as n,c as a,e,o as l}from"./app-CoWQeDNb.js";const p={};function i(t,s){return l(),a("div",null,[...s[0]||(s[0]=[e(`<h2 id="资源" tabindex="-1"><a class="header-anchor" href="#资源"><span>资源</span></a></h2><ul><li>项目主页：https://kestra.io/docs/tutorial/fundamentals</li><li>轻量灵活的命令行JSON处理器：https://jqlang.org/</li></ul><h2 id="工作流" tabindex="-1"><a class="header-anchor" href="#工作流"><span>工作流</span></a></h2><div class="language-yaml line-numbers-mode" data-highlighter="prismjs" data-ext="yml"><pre><code class="language-yaml"><span class="line"><span class="token key atrule">id</span><span class="token punctuation">:</span> getting_started</span>
+<span class="line"><span class="token key atrule">namespace</span><span class="token punctuation">:</span> company.team</span>
+<span class="line"></span>
+<span class="line"><span class="token key atrule">description</span><span class="token punctuation">:</span> <span class="token punctuation">|</span><span class="token scalar string"></span>
+<span class="line">  # Getting Started</span>
+<span class="line">  Let&#39;s \`write\` some **markdown** - [first flow](https://t.ly/Vemr0) 🚀</span></span>
+<span class="line"></span>
+<span class="line"><span class="token key atrule">labels</span><span class="token punctuation">:</span></span>
+<span class="line">  <span class="token key atrule">owner</span><span class="token punctuation">:</span> rick.astley</span>
+<span class="line">  <span class="token key atrule">project</span><span class="token punctuation">:</span> never<span class="token punctuation">-</span>gonna<span class="token punctuation">-</span>give<span class="token punctuation">-</span>you<span class="token punctuation">-</span>up</span>
+<span class="line"></span>
+<span class="line"><span class="token key atrule">tasks</span><span class="token punctuation">:</span></span>
+<span class="line">  <span class="token punctuation">-</span> <span class="token key atrule">id</span><span class="token punctuation">:</span> hello_world</span>
+<span class="line">    <span class="token key atrule">type</span><span class="token punctuation">:</span> io.kestra.plugin.core.log.Log</span>
+<span class="line">    <span class="token key atrule">message</span><span class="token punctuation">:</span> Hello World<span class="token tag">!</span></span>
+<span class="line">    <span class="token key atrule">description</span><span class="token punctuation">:</span> <span class="token punctuation">|</span><span class="token scalar string"></span>
+<span class="line">      ## About this task</span>
+<span class="line">      This task will print &quot;Hello World!&quot; to the logs.</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ul><li>id 表示流的名称。相同id的工作流，可存在于不同的namespace中。id 和 namespace 的组合用作流的唯一标识符 。</li><li>namespace 可用于分离开发和生产环境。</li><li>tasks 是将按定义的顺序执行的任务列表。推荐使用小写英文和下划线命名。</li></ul><ol><li>空间：创建流后，您将无法更改其命名空间。</li><li>标签：使用键值对对流进行分组</li></ol><h2 id="获取工作流的输入" tabindex="-1"><a class="header-anchor" href="#获取工作流的输入"><span>获取工作流的输入</span></a></h2><div class="language-yaml line-numbers-mode" data-highlighter="prismjs" data-ext="yml"><pre><code class="language-yaml"><span class="line"><span class="token key atrule">id</span><span class="token punctuation">:</span> getting_started</span>
+<span class="line"><span class="token key atrule">namespace</span><span class="token punctuation">:</span> company.team</span>
+<span class="line"></span>
+<span class="line"><span class="token key atrule">inputs</span><span class="token punctuation">:</span></span>
+<span class="line">  <span class="token punctuation">-</span> <span class="token key atrule">id</span><span class="token punctuation">:</span> api_url</span>
+<span class="line">    <span class="token key atrule">type</span><span class="token punctuation">:</span> STRING</span>
+<span class="line">    <span class="token key atrule">defaults</span><span class="token punctuation">:</span> https<span class="token punctuation">:</span>//dummyjson.com/products</span>
+<span class="line"></span>
+<span class="line"><span class="token key atrule">tasks</span><span class="token punctuation">:</span></span>
+<span class="line">  <span class="token punctuation">-</span> <span class="token key atrule">id</span><span class="token punctuation">:</span> api</span>
+<span class="line">    <span class="token key atrule">type</span><span class="token punctuation">:</span> io.kestra.plugin.core.http.Request</span>
+<span class="line">    <span class="token key atrule">uri</span><span class="token punctuation">:</span> <span class="token string">&quot;{{ inputs.api_url }}&quot;</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="获取任务的输出" tabindex="-1"><a class="header-anchor" href="#获取任务的输出"><span>获取任务的输出</span></a></h2><p>引用某个 <code>task_id(task-id)</code> 任务的输出:</p><div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre><code class="language-text"><span class="line">{{ outputs.task_id.output_property }}</span>
+<span class="line"></span>
+<span class="line"># 任务 ID 包含一个或多个连字符（- 符号），请将任务 ID 括在方括号中</span>
+<span class="line">{{ outputs[&#39;task-id&#39;].output_property }}</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>示例：</p><div class="language-yaml line-numbers-mode" data-highlighter="prismjs" data-ext="yml"><pre><code class="language-yaml"><span class="line"><span class="token key atrule">id</span><span class="token punctuation">:</span> getting_started</span>
+<span class="line"><span class="token key atrule">namespace</span><span class="token punctuation">:</span> company.team</span>
+<span class="line"></span>
+<span class="line"><span class="token key atrule">inputs</span><span class="token punctuation">:</span></span>
+<span class="line">  <span class="token punctuation">-</span> <span class="token key atrule">id</span><span class="token punctuation">:</span> api_url</span>
+<span class="line">    <span class="token key atrule">type</span><span class="token punctuation">:</span> STRING</span>
+<span class="line">    <span class="token key atrule">defaults</span><span class="token punctuation">:</span> https<span class="token punctuation">:</span>//dummyjson.com/products</span>
+<span class="line"></span>
+<span class="line"><span class="token key atrule">tasks</span><span class="token punctuation">:</span></span>
+<span class="line">  <span class="token punctuation">-</span> <span class="token key atrule">id</span><span class="token punctuation">:</span> api</span>
+<span class="line">    <span class="token key atrule">type</span><span class="token punctuation">:</span> io.kestra.plugin.core.http.Request</span>
+<span class="line">    <span class="token key atrule">uri</span><span class="token punctuation">:</span> <span class="token string">&quot;{{ inputs.api_url }}&quot;</span></span>
+<span class="line"></span>
+<span class="line">  <span class="token punctuation">-</span> <span class="token key atrule">id</span><span class="token punctuation">:</span> python</span>
+<span class="line">    <span class="token key atrule">type</span><span class="token punctuation">:</span> io.kestra.plugin.scripts.python.Script</span>
+<span class="line">    <span class="token key atrule">taskRunner</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token key atrule">type</span><span class="token punctuation">:</span> io.kestra.plugin.core.runner.Process</span>
+<span class="line">    <span class="token key atrule">beforeCommands</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token punctuation">-</span> pip install polars</span>
+<span class="line">    <span class="token key atrule">outputFiles</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token punctuation">-</span> <span class="token string">&quot;products.csv&quot;</span></span>
+<span class="line">    <span class="token key atrule">script</span><span class="token punctuation">:</span> <span class="token punctuation">|</span><span class="token scalar string"></span>
+<span class="line">      import polars as pl</span>
+<span class="line">      data = {{ outputs.api.body | jq(&#39;.products&#39;) | first }}</span>
+<span class="line">      df = pl.from_dicts(data)</span>
+<span class="line">      df.glimpse()</span>
+<span class="line">      df.select([&quot;brand&quot;, &quot;price&quot;]).write_csv(&quot;products.csv&quot;)</span></span>
+<span class="line"></span>
+<span class="line">  <span class="token punctuation">-</span> <span class="token key atrule">id</span><span class="token punctuation">:</span> sqlQuery</span>
+<span class="line">    <span class="token key atrule">type</span><span class="token punctuation">:</span> io.kestra.plugin.jdbc.duckdb.Query</span>
+<span class="line">    <span class="token key atrule">inputFiles</span><span class="token punctuation">:</span></span>
+<span class="line">      <span class="token key atrule">in.csv</span><span class="token punctuation">:</span> <span class="token string">&quot;{{ outputs.python.outputFiles[&#39;products.csv&#39;] }}&quot;</span></span>
+<span class="line">    <span class="token key atrule">sql</span><span class="token punctuation">:</span> <span class="token punctuation">|</span><span class="token scalar string"></span>
+<span class="line">      SELECT brand, round(avg(price), 2) as avg_price</span>
+<span class="line">      FROM read_csv_auto(&#39;{{ workingDir }}/in.csv&#39;, header=True)</span>
+<span class="line">      GROUP BY brand</span>
+<span class="line">      ORDER BY avg_price DESC;</span></span>
+<span class="line">    <span class="token key atrule">store</span><span class="token punctuation">:</span> <span class="token boolean important">true</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ol><li>使用 <code>{{ outputs.task_id.body }}</code> 语法，提取上游 <code>API</code> 任务的结果数据</li><li>使用 <a href="https://jqlang.org/" target="_blank" rel="noopener noreferrer">jq</a> 语法 <code>{{ outputs.task_id.body | jq(&#39;.products&#39;) | first }}</code>，提取 <code>products</code> 数组中的第一个元素</li><li>在执行实例的 <code>输出(Outputs)</code> 选项卡，对输出结果进行调试。</li><li>使用 <code>io.kestra.plugin.jdbc.duckdb.Query</code> 任务，对上游的 <code>CSV文件</code> 执行 SQL 查询，将结果作为可下载构件保存（配置：<code>store： true</code>）。</li></ol>`,14)])])}const u=n(p,[["render",i]]),o=JSON.parse('{"path":"/devops/kestra/inoutputs.html","title":"","lang":"zh-CN","frontmatter":{},"git":{"updatedTime":1751616128000,"contributors":[{"name":"Hankin","username":"Hankin","email":"whq78164@gmail.com","commits":1,"url":"https://github.com/Hankin"}],"changelog":[{"hash":"cd555a58414909a3498092ad3c0726cc124fa163","time":1751616128000,"email":"whq78164@gmail.com","author":"Hankin","message":"UPDATE kestra"}]},"filePathRelative":"devops/kestra/inoutputs.md"}');export{u as comp,o as data};
---
+++ assets/logrotate.html-Dz4ngFbi.js
@@ -0,0 +1,79 @@
+import{_ as n,c as a,e,o as l}from"./app-CoWQeDNb.js";const i={};function d(o,s){return l(),a("div",null,[...s[0]||(s[0]=[e(`<h2 id="logrotate介绍" tabindex="-1"><a class="header-anchor" href="#logrotate介绍"><span>logrotate介绍</span></a></h2><p>Logrotate 是 Linux 系统中用于自动化管理日志文件的工具，能够高效、安全地轮转、压缩、清理日志文件，防止磁盘空间被耗尽，同时保留必要的历史日志以便问题排查。</p><h2 id="安装" tabindex="-1"><a class="header-anchor" href="#安装"><span>安装</span></a></h2><div class="language-bash line-numbers-mode" data-highlighter="prismjs" data-ext="sh"><pre><code class="language-bash"><span class="line"><span class="token function">apt</span> <span class="token function">install</span> <span class="token function">logrotate</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div></div></div><h2 id="配置" tabindex="-1"><a class="header-anchor" href="#配置"><span>配置</span></a></h2><ul><li>rotate N 保留指定数量的历史日志（如 rotate 7 保留最近7份）。</li><li>maxage 删除超过天数的旧日志（如 maxage 30 删除30天前的文件）。</li><li>dateext：在日志名中添加日期后缀（如 app.log-20250508.gz），便于按时间归档。</li><li>olddir：将旧日志移动到指定目录（如 olddir /var/log/archive）。</li><li>weekly：按周进行轮转。优先级低于 size（如同时配置时，达到大小立即触发）。可用值：daily/weekly/monthly</li><li>size N 配置文件达到指定大小时触发轮转（如 <code>size 100M</code>）。</li><li>notifempty：空日志文件不轮转。</li></ul><h3 id="全局主配置文件" tabindex="-1"><a class="header-anchor" href="#全局主配置文件"><span>全局主配置文件</span></a></h3><p>路径：<code>/etc/logrotate.conf</code>，定义全局默认参数（如 compress、weekly）。</p><div class="language-conf line-numbers-mode" data-highlighter="prismjs" data-ext="conf"><pre><code class="language-conf"><span class="line"># see &quot;man logrotate&quot; for details</span>
+<span class="line"></span>
+<span class="line"># global options do not affect preceding include directives</span>
+<span class="line"></span>
+<span class="line"># rotate log files weekly</span>
+<span class="line">weekly</span>
+<span class="line"></span>
+<span class="line"># keep 4 weeks worth of backlogs</span>
+<span class="line">rotate 4</span>
+<span class="line"></span>
+<span class="line"># create new (empty) log files after rotating old ones</span>
+<span class="line">create</span>
+<span class="line"></span>
+<span class="line"># use date as a suffix of the rotated file</span>
+<span class="line">#dateext</span>
+<span class="line"></span>
+<span class="line"># uncomment this if you want your log files compressed</span>
+<span class="line">#compress</span>
+<span class="line"></span>
+<span class="line"># packages drop log rotation information into this directory</span>
+<span class="line">include /etc/logrotate.d</span>
+<span class="line"></span>
+<span class="line"># system-specific logs may also be configured here.</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="子配置文件" tabindex="-1"><a class="header-anchor" href="#子配置文件"><span>子配置文件</span></a></h3><p>路径：<code>/etc/logrotate.d/</code> 目录下的文件（如 <code>nginx</code>、<code>mysql</code>、<code>odoo</code>），针对特定服务覆盖全局设置。</p><p>在 <code>/etc/logrotate.d/</code>下创建配置文件（如odoo）：</p><p><code>/etc/logrotate.d/odoo</code>:</p><div class="language-conf line-numbers-mode" data-highlighter="prismjs" data-ext="conf"><pre><code class="language-conf"><span class="line"># /etc/logrotate.d/odoo文件</span>
+<span class="line"># /var/log/odoo/*.log {</span>
+<span class="line">/var/log/odoo/odoo.log {</span>
+<span class="line">    daily          # 按天轮转</span>
+<span class="line">    missingok      # 日志不存在时不报错</span>
+<span class="line">    rotate 30      # 保留30天的日志</span>
+<span class="line">    compress       # 压缩旧日志</span>
+<span class="line">    delaycompress  # 延迟压缩（下次轮转时压缩）</span>
+<span class="line">    notifempty     # 空日志不轮转</span>
+<span class="line">    create 640 odoo odoo  # 创建新日志文件（权限、用户、组）</span>
+<span class="line">    sharedscripts  # 脚本只执行一次</span>
+<span class="line">    postrotate</span>
+<span class="line">        # 向Odoo主进程发送USR1信号，通知其重新打开日志文件</span>
+<span class="line">        # kill -USR1 $(cat /var/run/odoo/odoo.pid 2&gt;/dev/null) 2&gt;/dev/null || true</span>
+<span class="line">        docker kill -s USR1 odoo_container_name</span>
+<span class="line"></span>
+<span class="line">        # 向Promtail发送HUP信号，通知其重新加载配置文件</span>
+<span class="line">        docker kill -s HUP promtail_container_name</span>
+<span class="line">    endscript</span>
+<span class="line">}</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><code>odoo.conf</code>:</p><div class="language-ini line-numbers-mode" data-highlighter="prismjs" data-ext="ini"><pre><code class="language-ini"><span class="line"><span class="token key attr-name">logfile</span> <span class="token punctuation">=</span> <span class="token value attr-value">/var/log/odoo/odoo.log</span></span>
+<span class="line"><span class="token comment"># Odoo默认可能不生成PID文件。添加pidfile</span></span>
+<span class="line"><span class="token key attr-name">pidfile</span> <span class="token punctuation">=</span> <span class="token value attr-value">/var/run/odoo/odoo.pid</span></span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>配置参数可以抽离公共部分：</p><p><code>/etc/logrotate.d/odoo</code>:</p><div class="language-conf line-numbers-mode" data-highlighter="prismjs" data-ext="conf"><pre><code class="language-conf"><span class="line"># 全局配置</span>
+<span class="line">daily</span>
+<span class="line">missingok</span>
+<span class="line">rotate 30</span>
+<span class="line">compress</span>
+<span class="line">delaycompress</span>
+<span class="line">notifempty</span>
+<span class="line">sharedscripts</span>
+<span class="line">su myname crontab</span>
+<span class="line">create 0660 sshd crontab</span>
+<span class="line"># 组名crontab已存在，无需修改</span>
+<span class="line">copytruncate</span>
+<span class="line"># 确保容器日志安全轮转</span>
+<span class="line">/home/myname/erp/odoo/log/odoo.log {</span>
+<span class="line">    postrotate</span>
+<span class="line">        docker kill -s USR1 odooweb      # 通知Odoo重载日志</span>
+<span class="line">        docker kill -s HUP promtail       # 通知Promtail重载配置</span>
+<span class="line">    endscript</span>
+<span class="line">}</span>
+<span class="line"># 测试环境日志</span>
+<span class="line">/home/myname/erp.test/odoo/log/odoo.log {</span>
+<span class="line">    postrotate</span>
+<span class="line">        docker kill -s USR1 odoowebtest  # 通知测试环境</span>
+<span class="line">        docker kill -s HUP promtail       # 通知Promtail</span>
+<span class="line">    endscript</span>
+<span class="line">}</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ol><li>日志所属目录 <code>不能</code> 为 <code>777</code>，可设置为 <code>770</code> 。否则 logrotate 会拒绝操作，提示：<code>权限不够</code></li><li>注释 <code>不要</code> 加在每行配置项后面。如：<code>create 0640 appuser appgroup # 注释</code>。否则会报错，参数过多。</li><li>su 配置指令，要确保能正常执行。把能运行的用户，加入用户组：<code>sudo usermod -aG crontab myname</code></li></ol><h2 id="使用" tabindex="-1"><a class="header-anchor" href="#使用"><span>使用</span></a></h2><h3 id="自动运行cron定时任务" tabindex="-1"><a class="header-anchor" href="#自动运行cron定时任务"><span>自动运行cron定时任务</span></a></h3><p>通过 <code>cron</code> 任务每日执行（路径：<code>/etc/cron.daily/logrotate</code>）</p><h3 id="手动执行" tabindex="-1"><a class="header-anchor" href="#手动执行"><span>手动执行</span></a></h3><div class="language-bash line-numbers-mode" data-highlighter="prismjs" data-ext="sh"><pre><code class="language-bash"><span class="line"><span class="token comment"># 调试模式：显示计划，不实际修改文件</span></span>
+<span class="line"><span class="token function">sudo</span> <span class="token function">logrotate</span> <span class="token parameter variable">-d</span> /etc/logrotate.d/odoo</span>
+<span class="line"></span>
+<span class="line"><span class="token comment"># 强制立即执行</span></span>
+<span class="line"><span class="token function">sudo</span> <span class="token function">logrotate</span> <span class="token parameter variable">-f</span> /etc/logrotate.d/odoo</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,25)])])}const p=n(i,[["render",d]]),r=JSON.parse('{"path":"/devops/logrotate.html","title":"","lang":"zh-CN","frontmatter":{},"git":{"updatedTime":1753254692000,"contributors":[{"name":"Hankin","username":"Hankin","email":"whq78164@gmail.com","commits":4,"url":"https://github.com/Hankin"}],"changelog":[{"hash":"6d7249246f4ea166453506b0552b6b2d6fe0e750","time":1753254692000,"email":"whq78164@gmail.com","author":"Hankin","message":"UPDATE promtail"},{"hash":"e8519aa41da5fe2f3877981a6c3e47115b07b22c","time":1752418311000,"email":"hankin@catmes.com","author":"Hankin","message":"ADD linux/systemd.md. UPDATE promtail"},{"hash":"2532d20cad7caa42d194628f1d1086e92212170a","time":1752227037000,"email":"whq78164@gmail.com","author":"Hankin","message":"UPDATE promtail, loki"},{"hash":"39d821a62f30206a59f608169caef96118afa4c4","time":1752206694000,"email":"whq78164@gmail.com","author":"Hankin","message":"ADD logrotate. ADD grafana, promtail, loki"}]},"filePathRelative":"devops/logrotate.md"}');export{p as comp,r as data};
